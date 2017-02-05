8294152fe520cf28be622d7328333a03b7d2a22e
James Fox <foxjas@berkeley.edu>
2017-02-05 00:40:54 -0500

    refactored command line interface (dropped output-type). implemented initial version of Louvain-based relabeling. refactored relabelGraphSnap() to call Louvain or Infomap file processing as helpers (better abstraction)

--- graph_relabeling.cpp
+++ graph_relabeling.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include <time.h>
+#include <assert.h>
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
@@ -31,12 +32,17 @@ bool hasOption(const char* option, int argc, char **argv) {
   return false;
 }
 
-// Driver function to sort the vector elements by second element of pairs
+// Driver function to sort the vector elements by second element of pairs, ascending order
+bool sortBySecAsec(const pair<int,int> &a, const pair<int,int> &b) {
+    return (a.second < b.second);
+}
+
+// Driver function to sort the vector elements by second element of pairs, descending order
 bool sortBySecDesc(const pair<int,int> &a, const pair<int,int> &b) {
     return (a.second > b.second);
 }
 
-void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_type, char* mappingPath) {
+void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mappingPath) {
     vertexId_t nv,*src,*dest;
     length_t   ne;
     nv = ne = -1;
@@ -88,19 +94,26 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_typ
     // write out relabeled graph to file
     ofstream fout;
     fout.open(relabeledGraphPath);
-    bool snap_output = (out_type == NULL || strcmp(out_type, "snap") == 0);
-    if (snap_output) {
+    string outFileName(relabeledGraphPath);
+    bool isSnap = outFileName.find(".txt")==std::string::npos?false:true;
+    bool isMarket = outFileName.find(".mtx")==std::string::npos?false:true;
+    if (isSnap) {
         printf("Outputting new SNAP graph\n");
         fout << "# Nodes: " << vertices.size() << " " << "Edges: " << ne << "\n";
-    } else {
+    } else if (isMarket) {
         printf("Outputting new matrix market graph\n");
         fout << vertices.size() << " " <<  vertices.size() << " " << ne << "\n";
+    } else {
+        cout << "Unrecognized output graph file type: defaulting to SNAP .txt" << endl;
+        printf("Outputting new SNAP graph\n");
+        fout << "# Nodes: " << vertices.size() << " " << "Edges: " << ne << "\n";
     }
+
     vertexId_t relabeledSrc, relabeledDest;
     for (vertexId_t i=0; i<counter; i++) {
         relabeledSrc = relabel_map[src[i]];
         relabeledDest = relabel_map[dest[i]];
-        if (snap_output) {
+        if (isSnap) {
             fout << relabeledSrc << " " << relabeledDest << "\n";
         } else {
             fout << relabeledSrc+1 << " " << relabeledDest+1 << "\n";
@@ -120,7 +133,76 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_typ
     free(dest);
 }
 
-void relabelGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_type, char* partitionInfoPath, char* mappingPath) {
+vector<vertexId_t> relabelVerticesFromInfomap(FILE *com_fp, int nv) {
+    // read in community-partitioned vertices
+    const int MAX_CHARS = 1000;
+    char temp[MAX_CHARS];
+    vertexId_t vid, i=0;
+    vector<vertexId_t> vertices(nv);
+    char* written = fgets(temp, MAX_CHARS, com_fp);
+    while (written != NULL && *temp == '#') { // skip comments
+        written = fgets(temp, MAX_CHARS, com_fp);
+    }
+    while (written != NULL) { // read vertices in-order
+        sscanf(temp, "%d %*s\n", (vertexId_t*)&vid);
+        vertices[i] = vid;
+        written = fgets(temp, MAX_CHARS, com_fp);
+        i += 1;
+    }
+    return vertices;
+}
+
+vector<vertexId_t> relabelVerticesFromLouvain(FILE *com_fp, int nv) {
+    vector<pair<vertexId_t, int>> vertex_labels;
+    vertex_labels.reserve(nv);
+    vertexId_t a_prev = -1;
+    vertexId_t a;
+    int b;
+    char *written;
+    const int MAX_CHARS = 100;
+    char temp[MAX_CHARS];
+    
+    // read in initial (vertex id, community id) pairs
+    written = fgets(temp, MAX_CHARS, com_fp);
+    sscanf(temp, "%d %d\n", (vertexId_t*)&a, (vertexId_t*)&b);
+    while((written != NULL) && a > a_prev) {
+        vertex_labels.push_back(make_pair(a, b));
+        a_prev = a;
+        written = fgets(temp, MAX_CHARS, com_fp);
+        sscanf(temp, "%d %d\n", (vertexId_t*)&a, (vertexId_t*)&b);
+    }
+    assert(vertex_labels.size() == nv);
+    sort(vertex_labels.begin(), vertex_labels.end(), sortBySecAsec);
+
+    // read in (community id, community id) pairs, and reorder vertices accordingly
+    vector<int> com_map;
+    while (written != NULL) { // loop condition over entire file
+        while ((written != NULL) && a>a_prev) { // iterating over a level
+            com_map.push_back(b);
+            a_prev = a;
+            written = fgets(temp, MAX_CHARS, com_fp);
+            sscanf(temp, "%d %d\n", (vertexId_t*)&a, (vertexId_t*)&b);
+        }
+        if (com_map.size()) { // condition to avoid reordering the 1st time we enter outer while loop
+            for (int i=0; i<vertex_labels.size(); i++) {
+                vertex_labels[i].second = com_map[vertex_labels[i].second];
+            }
+            stable_sort(vertex_labels.begin(), vertex_labels.end(), sortBySecAsec);
+            com_map.clear();
+        }
+        a_prev = -1;
+    } /** NOTE: ends up calculating & sorting one "useless" identity map at the last level */
+
+    vector<vertexId_t> final;
+    final.reserve(nv);
+    for (vector<pair<vertexId_t, int>>::iterator pair = vertex_labels.begin(); pair != vertex_labels.end(); pair++) {
+        final.push_back((*pair).first);
+    }
+    assert(final.size() == nv);
+    return final;
+}
+
+void relabelGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* partitionInfoPath, char* mappingPath) {
     vertexId_t nv,*src,*dest;
     length_t   ne;
     nv = ne = -1;
@@ -156,24 +238,25 @@ void relabelGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_
     }
     fclose (fp);
 
-    // read in community-partitioned vertices
-    vertexId_t vid, i=0;
-    vector<vertexId_t> vertices(nv);
+    vector<vertexId_t> vertices;
     fp = fopen(partitionInfoPath, "r");
-    written = fgets(temp, MAX_CHARS, fp);
-    while (written != NULL && *temp == '#') { // skip comments
-        written = fgets(temp, MAX_CHARS, fp);
-    }
-    while (written != NULL) { // read vertices in-order
-        sscanf(temp, "%d %*s\n", (vertexId_t*)&vid);
-        vertices[i] = vid;
-        written = fgets(temp, MAX_CHARS, fp);
-        i += 1;
+    string comFileName(partitionInfoPath);
+    bool isInfomap = comFileName.find(".clu")==std::string::npos?false:true;
+    bool isLouvain = comFileName.find(".tree")==std::string::npos?false:true;
+    // read in community-partitioned vertices
+    if (isInfomap) {
+        vertices = relabelVerticesFromInfomap(fp, nv);
+    } else if (isLouvain) {
+        vertices = relabelVerticesFromLouvain(fp, nv);
+    } else {
+        /** Error out */
+        cerr << "Error: could not recognize provided community label file" << endl;
+        exit(1);
     }
     fclose (fp);
 
     // create relabeling map
-    unordered_map<vertexId_t, vertexId_t> relabel_map;
+    unordered_map<vertexId_t, vertexId_t> relabel_map; /** NOTE: could replace this map with array */
     for (length_t i=0; i<nv; i++) {
         relabel_map[vertices[i]] = i;
     }
@@ -181,19 +264,25 @@ void relabelGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* out_
     // write out relabeled graph to file
     ofstream fout;
     fout.open(relabeledGraphPath);
-    bool snap_output = (out_type == NULL || strcmp(out_type, "snap") == 0);
-    if (snap_output) {
+    string outFileName(relabeledGraphPath);
+    bool isSnap = outFileName.find(".txt")==std::string::npos?false:true;
+    bool isMarket = outFileName.find(".mtx")==std::string::npos?false:true;
+    if (isSnap) {
         printf("Outputting new SNAP graph\n");
         fout << "# Nodes: " << vertices.size() << " " << "Edges: " << ne << "\n";
-    } else {
+    } else if (isMarket) {
         printf("Outputting new matrix market graph\n");
         fout << vertices.size() << " " <<  vertices.size() << " " << ne << "\n";
+    } else {
+        cout << "Unrecognized output graph file type: defaulting to SNAP .txt" << endl;
+        printf("Outputting new SNAP graph\n");
+        fout << "# Nodes: " << vertices.size() << " " << "Edges: " << ne << "\n";
     }
     vertexId_t relabeledSrc, relabeledDest;
     for (vertexId_t i=0; i<counter; i++) {
         relabeledSrc = relabel_map[src[i]];
         relabeledDest = relabel_map[dest[i]];
-        if (snap_output) {
+        if (isSnap) {
             fout << relabeledSrc << " " << relabeledDest << "\n";
         } else {
             fout << relabeledSrc+1 << " " << relabeledDest+1 << "\n";
@@ -277,17 +366,16 @@ int main(const int argc, char *argv[])
     char *output_graph_path = argv[2];
     char *partition_info_path = getOption("-p", argc, argv);
     char *mapping_path = getOption("-m", argc, argv);
-    char *out_type = getOption("-o", argc, argv);
     bool get_degrees = hasOption("--degrees", argc, argv);
 
     clock_t diff;
     clock_t start = clock();
     if (partition_info_path != NULL) {
-        relabelGraphSNAP(input_graph_path, output_graph_path, out_type, partition_info_path, mapping_path);
+        relabelGraphSNAP(input_graph_path, output_graph_path, partition_info_path, mapping_path);
     } else if (get_degrees) {
         writeDegreesFile(input_graph_path, output_graph_path);
     } else {
-        readGraphSNAP(input_graph_path, output_graph_path, out_type, mapping_path);
+        readGraphSNAP(input_graph_path, output_graph_path, mapping_path);
     }
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
