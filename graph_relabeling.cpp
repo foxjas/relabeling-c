0410a589d5ed4169c6ac4458f98589617c25dce8
James Fox <foxjas@berkeley.edu>
2017-02-16 15:20:35 -0500

    added undirected option to reading SNAP file

--- graph_relabeling.cpp
+++ graph_relabeling.cpp
@@ -16,28 +16,29 @@
 
 using namespace std;
 
-void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mappingPath) {
-    vertexId_t nv,*src,*dest;
-    length_t   ne;
-    nv = ne = -1;
+void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mappingPath, bool undirected) {
+    vertexId_t nv = -1;
+    length_t ne = -1;
     const int MAX_CHARS = 1000;
     char temp[MAX_CHARS];
     char *written;
     FILE *fp = fopen(inputGraphPath, "r");
 
     // scan for SNAP header comment
-    while (nv == -1 || ne == -1) {
-    	fgets(temp, MAX_CHARS, fp);
-    	sscanf(temp, "# Nodes: %d Edges: %d\n", &nv,&ne);
-    }
-
     written = fgets(temp, MAX_CHARS, fp);
+    while ((nv == -1 || ne == -1) && written != NULL) {
+    	sscanf(temp, "# Nodes: %d Edges: %d\n", &nv,&ne);
+            written = fgets(temp, MAX_CHARS, fp);
+    }
+    if ((nv == -1 || ne == -1) && written == NULL) {
+        fprintf(stderr, "SNAP input file is missing header info\n");
+        exit(-1);
+    }
     while (written != NULL && *temp == '#') { // skip any other comments
         written = fgets(temp, MAX_CHARS, fp);
     }
 
-    src = (vertexId_t *) malloc ((ne ) * sizeof (vertexId_t));
-    dest = (vertexId_t *) malloc ((ne ) * sizeof (vertexId_t));
+    vector<pair<vertexId_t, vertexId_t> >  edges(ne);
     unordered_set<vertexId_t> vertex_set;
     vertexId_t counter=0;
     vertexId_t srctemp,desttemp;
@@ -46,8 +47,11 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mapping
     while(counter<ne)
     {
         sscanf(temp, "%d %d\n", (vertexId_t*)&srctemp, (vertexId_t*)&desttemp);
-        src[counter]=srctemp;
-        dest[counter]=desttemp;
+        if (undirected) {
+            edges[counter]= make_pair(min(srctemp, desttemp), max(srctemp, desttemp));
+        } else {
+            edges[counter] = make_pair(srctemp, desttemp);
+        }
         vertex_set.insert(srctemp);
         vertex_set.insert(desttemp);
         counter++;
@@ -56,18 +60,43 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mapping
     fclose (fp);
     assert(counter == ne);
     assert(vertex_set.size() == nv);
+    printf("Original input: %d vertices, %d edges\n", nv, ne);
 
-    // get graph vertices, sorted
+    // convert to undirected edges, and remove potential duplicates
+    vector<pair<vertexId_t, vertexId_t> > edges_final;
+    if (undirected) {
+        sort(edges.begin(), edges.end(), sortByPairAsec);
+        vertexId_t prev_first = -1;
+        vertexId_t prev_second = -1;
+        vertexId_t first, second;
+        int duplicates = 0;
+        for (vector<pair<vertexId_t, vertexId_t> >::iterator pair = edges.begin(); pair != edges.end(); pair++) {
+            first = pair->first;
+            second = pair->second;
+            if (first == prev_first && second == prev_second) {
+                duplicates += 1;
+            } else {
+                edges_final.push_back(*pair);
+                edges_final.push_back(make_pair(second, first));
+            }
+            prev_first = first;
+            prev_second = second;
+        }
+        ne = edges_final.size();
+        printf("Removed %d duplicate edges in conversion to undirected\n", duplicates);
+    } else {
+        edges_final = edges;
+    }
+
+    // sort graph vertices and use to create relabeling map
     vector<vertexId_t> vertices(vertex_set.begin(), vertex_set.end());
     sort(vertices.begin(), vertices.end());
-
-    // create relabeling map
     unordered_map<vertexId_t, vertexId_t> relabel_map;
     for (length_t i=0; i<nv; i++) {
     	relabel_map[vertices[i]] = i;
     }
 
-    // write out relabeled graph to file
+    // relabel & write out relabeled graph to file
     ofstream fout;
     fout.open(relabeledGraphPath);
     string outFileName(relabeledGraphPath);
@@ -84,13 +113,12 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mapping
         printf("Outputting new SNAP graph\n");
         fout << "# Nodes: " << vertices.size() << " " << "Edges: " << ne << "\n";
     }
-
     vertexId_t relabeledSrc, relabeledDest;
-    for (vertexId_t i=0; i<counter; i++) {
-        relabeledSrc = relabel_map[src[i]];
-        relabeledDest = relabel_map[dest[i]];
-        assert(relabeledSrc >= 0 && relabeledSrc < nv);
-        assert(relabeledDest >= 0 && relabeledDest < nv);
+    for (vertexId_t i=0; i<ne; i++) {
+        relabeledSrc = relabel_map[edges_final[i].first];
+        relabeledDest = relabel_map[edges_final[i].second];
+        // assert(relabeledSrc >= 0 && relabeledSrc < nv);
+        // assert(relabeledDest >= 0 && relabeledDest < nv);
         if (isSnap) {
             fout << relabeledSrc << " " << relabeledDest << "\n";
         } else {
@@ -99,6 +127,7 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mapping
     }
     fout.close();
 
+    // write out re-mapping file
     if (mappingPath != NULL) {
         fout.open(mappingPath);
         fout << "New ID Old ID\n";
@@ -106,9 +135,7 @@ void readGraphSNAP(char* inputGraphPath, char* relabeledGraphPath, char* mapping
             fout << i << " " << vertices[i] << "\n";
     }
 
-    printf("Processed %lu vertices, %d edges as input\n", vertices.size(), counter);
-    free(src);
-    free(dest);
+    printf("Processed %lu vertices, %d edges\n", vertices.size(), ne);
 }
 
 vector<vertexId_t> relabelVerticesFromInfomap(FILE *com_fp, int nv) {
@@ -295,6 +322,7 @@ int main(const int argc, char *argv[])
     char *mapping_path = getOption("-m", argc, argv);
     bool get_degrees = hasOption("--degrees", argc, argv);
     bool get_com_stats = hasOption("--community", argc, argv);
+    bool undirected = hasOption("--undirected", argc, argv);
     bool edge_dups = hasOption("--duplicates", argc, argv);
 
     clock_t diff;
@@ -308,7 +336,7 @@ int main(const int argc, char *argv[])
     } else if (edge_dups) {
         printf("Duplicated eges: %d\n", checkDuplicateEdges(input_graph_path));
     } else {
-        readGraphSNAP(input_graph_path, output_graph_path, mapping_path);
+        readGraphSNAP(input_graph_path, output_graph_path, mapping_path, undirected);
     }
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
